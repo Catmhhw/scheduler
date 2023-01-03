7beb77c3499bbe9dbca2cd46915e36a746124057
Catmhhw <catherine.mhh.wu@gmail.com>
2023-01-03 02:15:44 -0500

    refactor with onChange and value

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,16 +1,24 @@
 import React from "react";
 import InterviewerListItem from "components/InterviewerListItem";
+import "components/InterviewerListItem.scss";
 
 export default function InterviewerList(props) {
 
 	const listInterviewers = props.interviewers.map((interviewer) => {
 		return (
+			// <InterviewerListItem
+			// 	key={interviewer.id}
+			// 	name={interviewer.name}
+			// 	avatar={interviewer.avatar}
+			// 	selected={interviewer.id === props.interviewer}
+			// 	setInterviewer={() => props.setInterviewer(interviewer.id)}
+			// />
 			<InterviewerListItem
 				key={interviewer.id}
 				name={interviewer.name}
 				avatar={interviewer.avatar}
-				selected={interviewer.id === props.interviewer}
-				setInterviewer={() => props.setInterviewer(interviewer.id)}
+				selected={interviewer.id === props.value}
+				setInterviewer={() => props.onChange(interviewer.id)}
 			/>
 		)
 	});
