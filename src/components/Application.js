767cdf2179654ed1d1e4bc802e02211a05ce1b1d
Catmhhw <catherine.mhh.wu@gmail.com>
2023-02-03 18:13:05 -0500

    add cancelInterview function

--- src/components/Application.js
+++ src/components/Application.js
@@ -39,10 +39,25 @@ export default function Application(props) {
       ...state.appointments,
       [id]: appointment
     };
-    
-    console.log(id, interview);
+
     return axios.put(`/api/appointments/${id}`, { interview })
-    .then(setState({ ...state, appointments }));
+      .then(setState({ ...state, appointments }));
+  }
+
+
+  function cancelInterview(id) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: null
+    };
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    return axios.delete(`/api/appointments/${id}`)
+      .then(setState({ ...state, appointments }));
   }
 
 
@@ -59,6 +74,7 @@ export default function Application(props) {
         interview={interview}
         interviewers={interviewers}
         bookInterview={bookInterview}
+        cancelInterview={cancelInterview}
       />
     );
   });
